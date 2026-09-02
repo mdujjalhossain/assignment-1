e7d38e74b2ca49f97f430154a8e1c41578e42e5d
mdujjalhossain <mdujjalhossain.cse@gmail.com>
2026-09-02 10:17:55 +0600

    question 2 solved

--- answer.js
+++ answer.js
@@ -1,1 +1,23 @@
+
 // Question-> 2
+
+function getDayData(day){
+    let input_day = day.toLowerCase()
+
+    switch(input_day){
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+        
+        default:
+            return "Invalid Day";
+        
+    }
+}
